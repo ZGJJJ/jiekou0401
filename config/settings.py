b891ec9fd630dad3ce96f332b77d5baff4027a92
zhaogaojie <807756164@qq.com>
2025-04-01 17:07:55 +0800

    1

--- config/settings.py
+++ config/settings.py
@@ -8,6 +8,13 @@ FLASK_ENV = 'development'
 JWT_SECRET = secrets.token_hex(32)
 
 # 数据库配置
+# DB_CONFIG = {
+#     'host': "10.32.151.4",
+#     'port': "5432",
+#     'database': "inspur",
+#     'user': "inspur",
+#     'password': "lcjg$$9999"
+# }
 DB_CONFIG = {
     'host': "10.32.151.4",
     'port': "5432",
